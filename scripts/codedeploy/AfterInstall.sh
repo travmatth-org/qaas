e55efc404840f78f4a52689f5f88f98677002138
Travis Matthews <trav.matth@gmail.com>
2020-07-22 16:25:36 -0700

    mv privileged port access in codedeploy lifecycle hook

--- scripts/codedeploy/AfterInstall.sh
+++ scripts/codedeploy/AfterInstall.sh
@@ -2,3 +2,6 @@
 
 # give service appropriate permissions
 sudo chmod 755 /usr/sbin/httpd
+
+# allow service to run on privileged port
+sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/httpd
--- scripts/s3_user_data.sh
+++ scripts/s3_user_data.sh
@@ -1,21 +1,18 @@
 #!/bin/bash -xe
 
-# send script output to /tmp/userdata.log for debugging
-exec >> /tmp/userdata.log 2>&1
+# send script output to /tmp/user_data.log for debugging
+exec >> /tmp/user_data.log 2>&1
 
 # install dependencies
 sudo yum -y update
-sudo yum -y install ruby wget libcap2-bin
+sudo yum -y install ruby wget libcap2-bin shadow-utils.x86_64
 
 # install codedeploy agent
 cd /home/ec2-user
 wget https://aws-codedeploy-us-west-1.s3.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./intall auto
+sudo ./install auto
 sudo service codedeploy-agent status
 
 # create user for faas service
-sudo useradd faas -s /sbin/nogin -M
-
-# allow faas service to run on privileged port
-sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/httpd
+sudo useradd -s /sbin/nologin -M faas
