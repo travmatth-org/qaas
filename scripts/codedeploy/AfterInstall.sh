cacdce2c7be1796e71397bc372521517d9ad285c
Travis Matthews <trav.matth@gmail.com>
2020-07-25 16:17:12 -0700

    troubleshooting codedeploy hooks

--- scripts/codedeploy/AfterInstall.sh
+++ scripts/codedeploy/AfterInstall.sh
@@ -8,3 +8,6 @@ sudo chmod 755 /usr/sbin/httpd
 
 # allow service to run on privileged port
 sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/httpd
+
+# enable faas service
+sudo systemctl enable httpd
--- scripts/codedeploy/ApplicationStart.sh
+++ scripts/codedeploy/ApplicationStart.sh
@@ -3,5 +3,4 @@
 # send script output to /tmp/ApplicationStart.log for debugging
 exec >> /tmp/ApplicationStart.log 2>&1
 
-sudo systemctl enable httpd
 sudo systemctl start httpd
