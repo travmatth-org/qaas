e85f71d3f75dd8c30becc200361d1bdaeab4ad1f
Travis Matthews <trav.matth@gmail.com>
2020-08-13 20:51:55 -0700

    try allowing sysd to manage port privilege

--- scripts/codedeploy/AfterInstall.sh
+++ scripts/codedeploy/AfterInstall.sh
@@ -6,6 +6,6 @@ sudo rm /srv/assets.zip
 # shellcheck disable=SC2046
 sudo chmod 0444 $(find /srv -type f)
 # allow service to run on privileged port
-sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/httpd
+#sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/httpd
 # enable faas service
 sudo systemctl enable httpd
