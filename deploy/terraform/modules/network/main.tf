de68f285c95beb3d89a40fbbad2415477fa76c27
Travis Matthews <trav.matth@gmail.com>
2020-08-19 22:00:18 -0700

    add network acl

--- deploy/terraform/modules/network/main.tf
+++ deploy/terraform/modules/network/main.tf
@@ -39,65 +39,120 @@ output "public_subnet" {
 	value = aws_subnet.public_subnet
 }
 
-# resource "aws_network_acl" "public_subnet_acl" {
-# 	vpc_id = aws_vpc.local.id
-# 	subnet_ids = [aws_subnet.public_subnet.id]
+resource "aws_network_acl" "public_acl" {
+	vpc_id = aws_vpc.local.id
+	subnet_ids = [aws_subnet.public_subnet.id]
 
-	# ingress all
-	# ingress {
-	# 	protocol	= "tcp"
-	# 	rule_no		= 100
-	# 	action		= "allow"
-	# 	cidr_block	= "0.0.0.0/0"
-	# 	from_port	= 0
-	# 	to_port		= 65535
-	# }
+	tags = {
+		FaaS = "true"
+	}
+}
 
-	# egress all
-	# ingress {
-	# 	protocol	= "tcp"
-	# 	rule_no		= 200
-	# 	action		= "allow"
-	# 	cidr_block	= "0.0.0.0/0"
-	# 	from_port	= 0
-	# 	to_port		= 65535
-	# }
+resource "aws_network_acl_rule" "http_in" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 100
+	egress = false
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 80
+	to_port = 80
+	cidr_block	= "0.0.0.0/0"
+}
 
+resource "aws_network_acl_rule" "http_out" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 100
+	egress = true
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 80
+	to_port = 80
+	cidr_block	= "0.0.0.0/0"
+}
 
-	# ssh ingress, port 22
-	# ingress {
-	# 	protocol = "tcp"
-	# 	rule_no = 100
-	# 	action = "allow"
-	# 	cidr_block = "0.0.0.0/0"
-	# 	from_port = 22
-	# 	to_port = 22
-	# }
+resource "aws_network_acl_rule" "ssh_in" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 110
+	egress = false
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 22
+	to_port = 22
+	cidr_block	= "0.0.0.0/0"
+}
 
-	# http ingress, port 80
-	# ingress {
-	# 	protocol = "tcp"
-	# 	rule_no = 101
-	# 	action = "allow"
-	# 	cidr_block = "0.0.0.0/0"
-	# 	from_port = 80
-	# 	to_port = 80
-	# }
+resource "aws_network_acl_rule" "ssh_out" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 110
+	egress = true
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 22
+	to_port = 22
+	cidr_block	= "0.0.0.0/0"
+}
 
-	# egress, ephemeral ports
-	# egress {
-	# 	protocol = "tcp"
-	# 	rule_no = 300
-	# 	action = "allow"
-	# 	cidr_block = "0.0.0.0/0"
-	# 	from_port = 1024
-	# 	to_port = 65535
-	# }
+resource "aws_network_acl_rule" "https_in" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 120
+	egress = false
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 443
+	to_port = 443
+	cidr_block	= "0.0.0.0/0"
+}
 
-# 	tags = {
-# 		FaaS = "public-acl"
-# 	}
-# }
+resource "aws_network_acl_rule" "https_out" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 120
+	egress = true
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 443
+	to_port = 443
+	cidr_block	= "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "ephemeral_in" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 130
+	egress = false
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 1024
+	to_port = 65535
+	cidr_block	= "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "ephemeral_out" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 130
+	egress = true
+	protocol = "tcp"
+	rule_action = "allow"
+	from_port = 1024
+	to_port = 65535
+	cidr_block	= "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "deny_in" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 1000
+	egress = false
+	protocol = "-1"
+	rule_action = "deny"
+	cidr_block	= "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "deny_out" {
+	network_acl_id = aws_network_acl.public_acl.id
+	rule_number = 1000
+	egress = true
+	protocol = "-1"
+	rule_action = "deny"
+	cidr_block	= "0.0.0.0/0"
+}
 
 resource "aws_route_table" "faas_route_table" {
 	vpc_id = aws_vpc.local.id
