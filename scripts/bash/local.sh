382aef2b5905022a8950463bca72c56d610a2b89
Travis Matthews <trav.matth@gmail.com>
2020-08-19 18:16:45 -0700

    linting bash files

--- scripts/bash/local.sh
+++ scripts/bash/local.sh
@@ -1,3 +1,8 @@
-remove_host() {
-	ssh-keygen -R $(make tf.ip);
+# shellcheck disable=SC2148
+remove-host() {
+	ssh-keygen -R "$(make tf.ip)";
+}
+
+login-ec2() {
+	ssh -i protected/faas_ec2.key ec2-user@"$(make tf.ip)";
 }
